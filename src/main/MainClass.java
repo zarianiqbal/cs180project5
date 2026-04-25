65ca4b780e61c8e76da475302632605b552fc680
Zarian Iqbal <zarian@Zarians-MacBook-Pro.local>
2026-04-24 21:15:45 -0400

    Pushing all the files

---
+++ src/main/MainClass.java
@@ -0,0 +1,25 @@
+package main;
+// Episode #1 - DONE
+// Episode #2 - DONE
+// Episode #3 - DONE
+// Episode #4 - DONE
+// Episode #5 - DONE
+// Episode #6 - DONE
+// Episode #7 - DONE
+// Episode #8 - DONE
+// Episode #9 - DONE
+// Episode #10 - DONE
+// Episode #11 - DONE
+// Episode #12 - DONE
+// Episode #13 Part 1 - DONE
+// Episode #13 Part 2 - DONE
+// Episode #14 - DONE
+// Episode #15 - DONE
+// Episode #16 Part 1, Part 2, Part 3 and FINALE - DONE
+// Episode #17 - DEBUGGING
+public class MainClass {
+    public static void  main(String[]args){
+
+        new Game();
+    }
+}
---
+++ src/ui/GameOverOverlay.java
@@ -0,0 +1,64 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import gamestates.Gamestate;
+import gamestates.Playing;
+import main.Game;
+import static utilz.Constants.UI.URMButtons.*;
+
+public class GameOverOverlay {
+
+    private Playing playing;
+    private UrmButton restartButton;
+
+    public GameOverOverlay(Playing playing) {
+        this.playing = playing;
+        restartButton = new UrmButton(Game.GAME_WIDTH / 2 - URM_SIZE / 2, (int) (300 * Game.SCALE), URM_SIZE, URM_SIZE, 1);
+    }
+
+    public void update() {
+        restartButton.update();
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(new Color(0, 0, 0, 200));
+        g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
+
+        g.setColor(Color.white);
+        g.drawString("Game Over", Game.GAME_WIDTH / 2, 150);
+        g.drawString("Press esc to return to Main Menu", Game.GAME_WIDTH / 2, 270);
+
+        restartButton.draw(g);
+    }
+
+    public void mousePressed(MouseEvent e) {
+        if (restartButton.getBounds().contains(e.getX(), e.getY()))
+            restartButton.setMousePressed(true);
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        if (restartButton.getBounds().contains(e.getX(), e.getY()))
+            if (restartButton.isMousePressed()) {
+                playing.resetAll();
+                Gamestate.state = Gamestate.PLAYING;
+            }
+        restartButton.resetBools();
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        restartButton.setMouseOver(false);
+        if (restartButton.getBounds().contains(e.getX(), e.getY()))
+            restartButton.setMouseOver(true);
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            playing.resetAll();
+            Gamestate.state = Gamestate.MENU;
+        }
+    }
+}
